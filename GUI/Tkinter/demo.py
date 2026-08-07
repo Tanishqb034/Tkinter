5cad1a27f65bfcfb6ab5faa6b0569b20dea90f7a
Tanishq <tanishqb034@gmail.com>
2026-08-07 19:45:08 +0530

    THIS IS CHANGE OF BG

--- GUI/Tkinter/demo.py
+++ GUI/Tkinter/demo.py
@@ -2,5 +2,6 @@ from tkinter import *
 win =Tk()
 win.title("***Unique Computers ***")
 win.iconbitmap(r"C:\Users\tanis\Downloads\images.ico")
-win.attributes('-alpha',0.1) #this is trancparancy
+win.attributes('-alpha',1) #this is trancparancy
+win.config(bg="pink")  #change Background colour
 win.mainloop()
