7643fbc0c724c03e7ca939772ded396396bd3a67
Tanishq <tanishqb034@gmail.com>
2026-08-07 19:41:58 +0530

    THIS IS TRANCPARANCY

--- GUI/Tkinter/demo.py
+++ GUI/Tkinter/demo.py
@@ -2,4 +2,5 @@ from tkinter import *
 win =Tk()
 win.title("***Unique Computers ***")
 win.iconbitmap(r"C:\Users\tanis\Downloads\images.ico")
+win.attributes('-alpha',0.1) #this is trancparancy
 win.mainloop()
